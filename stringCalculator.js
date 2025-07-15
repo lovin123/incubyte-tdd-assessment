00046c8dcb619f2c76186afce61cb84a92cc1737
lovintanwar123 <lovin.tanwar@kollegeapply.com>
2025-07-15 15:25:48 +0530

    Refactored add function

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,11 +1,11 @@
-// String Calculator implementation
-
 export function add(numbers) {
   if (!numbers) return 0;
-  return numbers
+
+  const numberList = numbers
     .split(",")
     .map((n) => n.trim())
     .filter((n) => n.length > 0)
-    .map(Number)
-    .reduce((sum, n) => sum + n, 0);
+    .map(Number);
+
+  return numberList.reduce((sum, n) => sum + n, 0);
 }
